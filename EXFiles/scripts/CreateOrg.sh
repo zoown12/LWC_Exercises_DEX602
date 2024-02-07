91b9269d4b62d2704e88c63a164625df02730f1f
zoown12 <zoown12@gmail.com>
2024-02-07 11:10:52 +0900

    inital

--- EXFiles/scripts/CreateOrg.sh
+++ EXFiles/scripts/CreateOrg.sh
@@ -1,6 +1,6 @@
 # Execute in Mac using: ./EXFiles/scripts/CreateOrg.sh
 echo "*** Creating scratch Org..."
-sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias soDEX602 -d 30
+sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias soDEX602_1 -d 30
 echo "*** Opening scratch Org..."
 sfdx force:org:open
 echo "*** Pushing metadata to scratch Org..."
@@ -12,3 +12,5 @@ sfdx force:apex:execute -f EXFiles/data/CreateUsers.txt
 echo "*** Creating data"
 # sfdx ETCopyData:import -c EXFiles/data --loglevel warn
 sfdx force:apex:execute -f EXFiles/data/DeleteAndLoadData.txt
+
+#$ ./EXFiles/scripts/CreateOrg.sh
