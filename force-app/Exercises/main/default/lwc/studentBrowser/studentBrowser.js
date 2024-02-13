78ba0b6c7e3f23f47a4bdf978fbc0ad4fac44060
zoown12 <zoown12@gmail.com>
2024-02-13 10:03:20 +0900

    EX2-5 complete!

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
 
-export default class StudentBrowser extends LightningElement {}
+export default class StudentBrowser extends LightningElement {
+
+    studentList = [];
+
+    constructor(){
+        super();
+        const studentNames = ['Rad', 'Stuar', 'Andres', 'Jonas', 'Simon'];
+        this.studentList = studentNames.map((studentName,index) =>{
+            return {'sobjectType' : 'Contact',
+                    'Name' : studentName,
+                    'PhotoUrl' : '/services/images/photo/003B0FakePictId',
+                    'Id' : index
+            }
+        });
+    }
+}
--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -2,7 +2,9 @@ import { LightningElement ,api} from 'lwc';
 
 export default class StudentTile extends LightningElement {
 
-
+    studentClick(){
+        alert(this.student.Name);
+    }
     @api student = {
         Name : '[Your Name]',
         PhotoUrl : '/services/images/photo/003B0FakePictId',
