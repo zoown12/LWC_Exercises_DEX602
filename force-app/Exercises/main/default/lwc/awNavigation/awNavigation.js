9379678cc1eaade6506d58029bb412dfa051a024
zoown12 <zoown12@gmail.com>
2024-02-16 11:19:58 +0900

    4-1 complete!

---
+++ force-app/Exercises/main/default/lwc/awNavigation/awNavigation.js
@@ -0,0 +1,32 @@
+import { LightningElement,wire} from 'lwc';
+import getCertifications  from '@salesforce/apex/AwNavigation.getCertifications';
+
+export default class AwNavigation extends LightningElement{
+
+    certifications = [];
+    error;
+
+    
+    @wire(getCertifications)
+    wired_getCertifications({ error, data }) {
+        this.certifications = [];
+        if(data){
+            this.certifications = data.map(cert => (
+                {
+                Id:cert.Id,
+                Name:cert.Name,
+                compoundKey:`certification|${cert.Id}|${cert.Name}`
+            }));
+        }else if(error){
+            this.error = error;
+        }
+    }
+
+    onselect(event) {
+        const selectedItemName = event.detail.name;
+        const evt = new CustomEvent('navitemselected', {
+        detail: { itemName: selectedItemName }
+        });
+        this.dispatchEvent(evt);
+        }
+}
